Fix broadcasting and shape issues

src/automatic_diff_engine/tensor/tensor.py
@@ -20,7 +20,8 @@ class Tensor:
             requires_grad = True,
     ):
         data = np.atleast_1d(np.array(data, dtype=np.float64))
-        self.data = TensorData(data, np.zeros_like(data, dtype=np.float64), requires_grad)
+        grad = np.zeros_like(data, dtype=np.float64)
+        self.data = TensorData(data, grad, requires_grad)
         self.creator_func = creator_func
         self.creator_operands = creator_operands if creator_operands is not None else []
 
@@ -48,7 +49,10 @@ class Tensor:
 
             for i in range(len(tensor.creator_operands)):
                 if tensor.creator_operands[i].requires_grad:
-                    tensor.creator_operands[i].grad += grads[i]
+                    if tensor.creator_operands[i].grad is None:
+                        tensor.creator_operands[i].grad = grads[i]
+                    else:
+                        tensor.creator_operands[i].grad = tensor.creator_operands[i].grad + grads[i]
 
     def __repr__(self) -> str:
         return f"Tensor: {self.data}"
@@ -129,4 +133,4 @@ class Tensor:
 
     @property
     def shape(self):
-        return np.shape(self.data)
+        return self.value.shape

src/automatic_diff_engine/tensor/tensor_data.py
@@ -1,11 +1,15 @@
+from typing import Self, Union
+
 import numpy as np
 
 
 class TensorData:
     def __init__(self, value, grad, requires_grad):
-        self.value = np.array(value)
-        self.grad = np.array(grad)
+        self.value = value
+        self.grad = grad
         self.requires_grad = requires_grad
 
-    def __eq__(self, other):
-        return self.value == other.value
+    def __eq__(self, other: Union[Self, np.ndarray]) -> bool:
+        if isinstance(other, TensorData):
+            return np.array_equal(self.value, other.value)
+        return np.array_equal(self.value, other)

tests/automatic_diff_engine/tensor/functions/matrix_multiplication_test.py
@@ -4,8 +4,8 @@ from automatic_diff_engine.tensor.tensor import Tensor
 
 
 def test_forward():
-    matrix1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    matrix2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    matrix1 = np.array([[1, 2], [4, 5], [7, 8]])
+    matrix2 = np.array([[4, 5, 6], [7, 8, 9]])
 
     tensor1 = Tensor(matrix1)
     tensor2 = Tensor(matrix2)